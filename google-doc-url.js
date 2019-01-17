32408e6465e163cbfabeec7a7c7cc2e433d024b7
Moses Katembu <mose.kate@gmail.com>
2019-01-17 10:26:32 +0300

    Change data source

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ITNl1VwKxZxsOQ5Cq5zL6aP8n7CKkTI4xpK9-WP2e-E/edit#gid=0';
